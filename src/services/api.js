da351cb31cfdbe41f853b0b3d385d984b50d84f3
Liza <lizapikovskaya@gmail.com>
2023-06-07 23:29:10 +0300

    replace state to App

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
-export const fetchImages = (value, page ) => {
+export const fetchImages = (value, page = 1) => {
   const BASE_URL = 'https://pixabay.com/api';
   const API_KEY = '35736305-5d79b99fc6e7e7bd6a57f0349';
   return fetch(
     `${BASE_URL}/?key=${API_KEY}&q=${value}&page=${page}&per_page=12`
   );
 };
-
